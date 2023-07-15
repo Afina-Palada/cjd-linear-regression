cddec279e8b927b50e339f9b8b4a271c4f6dd62c
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2023-07-16 01:47:36 +0300

    Add trained models

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
+import pickle
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
 
-file1_data = pd.read_csv('СВОД 2022.csv')
-file2_data = pd.read_csv('СВОД 2023 6 мес.csv')
-data = pd.concat([file1_data, file2_data], ignore_index=True).drop(['сумма по билетам', 'сумма по плацкарту'], axis=1)
-data = pd.get_dummies(data, columns=['поезд', 'станция отправления', 'станция назначения', 'тип'])
+def predict_price(new_data):
+    with open('trained_model.pkl', 'rb') as file:
+        model, scaler, columns = pickle.load(file)
 
-X = data.drop(['итоговая сумма билета'], axis=1)
-y = data['итоговая сумма билета']
+    new_data_encoded = pd.get_dummies(new_data, columns=['поезд', 'станция отправления', 'станция назначения', 'тип'])
+    new_data_encoded = new_data_encoded.reindex(columns=columns, fill_value=0)
+    new_data_normalized = scaler.transform(new_data_encoded)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    predicted_price = round(model.predict(new_data_normalized)[0], 1)
+    return predicted_price
 
-scaler = MinMaxScaler()
-X_train_normalized = scaler.fit_transform(X_train)
 
-model = LinearRegression()
-model.fit(X_train_normalized, y_train)
+if __name__ == '__main__':
+    file1_data = pd.read_csv('СВОД 2022.csv')
+    file2_data = pd.read_csv('СВОД 2023 6 мес.csv')
+    data = pd.concat([file1_data, file2_data], ignore_index=True).drop(['сумма по билетам', 'сумма по плацкарту'], axis=1)
+    data = pd.get_dummies(data, columns=['поезд', 'станция отправления', 'станция назначения', 'тип'])
 
-mse = round(model.score(scaler.transform(X_test), y_test) * 100)
-print(f"Точность: {mse}%")
+    X = data.drop(['итоговая сумма билета'], axis=1)
+    y = data['итоговая сумма билета']
 
-new_data = pd.DataFrame({
-    'поезд': ['0306С/А'],
-    'месяц поездки': [1],
-    'месяц': [1],
-    'станция отправления': ['АДЛЕР'],
-    'станция назначения': ['МОСКВА КАЗ'],
-    'тип': ['П/3Б'],
-    'количество мест': [1],
-    'сумма сервисных услуг': [163.4],
-    'расстояние': [1790]
-})
-new_data_encoded = pd.get_dummies(new_data, columns=['поезд', 'станция отправления', 'станция назначения', 'тип'])
-new_data_encoded = new_data_encoded.reindex(columns=X.columns, fill_value=0)
-new_data_normalized = scaler.transform(new_data_encoded)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-predicted_price = round(model.predict(new_data_normalized)[0], 1)
-print(f"Предсказанная цена билета: {predicted_price}")
+    scaler = MinMaxScaler()
+    X_train_normalized = scaler.fit_transform(X_train)
+
+    model = LinearRegression()
+    model.fit(X_train_normalized, y_train)
+
+    mse = round(model.score(scaler.transform(X_test), y_test) * 100)
+    print(f"Точность: {mse}%")
+
+    with open('trained_model.pkl', 'wb') as file:
+        pickle.dump((model, scaler, X.columns), file)
+
+    new_data = pd.DataFrame({
+        'поезд': ['0306С/А'],
+        'месяц поездки': [1],
+        'месяц': [1],
+        'станция отправления': ['АДЛЕР'],
+        'станция назначения': ['МОСКВА КАЗ'],
+        'тип': ['П/3Б'],
+        'количество мест': [1],
+        'сумма сервисных услуг': [163.4],
+        'расстояние': [1790]
+    })
+    predicted_price = predict_price(new_data)
+    print(f"Предсказанная цена билета: {predicted_price}")
--- main2.py
+++ main2.py
@@ -2,46 +2,61 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle
 
-files = []
-file_names = ['data.csv', 'data2.csv', 'data3.csv', 'data4.csv', 'data5.csv']
-for file_name in file_names:
-    files.append(pd.read_csv(file_name))
 
-data = pd.concat(files, ignore_index=True).drop([
-    'Сумма по бил.',
-    'Сумма по плац.',
-    'Поезд/Нитка',
-    'Станция отп.',
-    'Станция назн.'
-], axis=1)
-data = pd.get_dummies(data, columns=['Тип/Кл.обсл.'])
+def predict_price(new_data):
+    with open('trained_model2.pkl', 'rb') as file:
+        model, scaler, columns = pickle.load(file)
 
-X = data.drop('Итого сумма билета', axis=1)
-y = data['Итого сумма билета']
+    new_data_encoded = pd.get_dummies(new_data, columns=['Тип/Кл.обсл.'])
+    new_data_encoded = new_data_encoded.reindex(columns=columns, fill_value=0)
+    new_data_normalized = scaler.transform(new_data_encoded)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    predicted_price = round(model.predict(new_data_normalized)[0], 1)
+    return predicted_price
 
-scaler = MinMaxScaler()
-X_train_normalized = scaler.fit_transform(X_train)
 
-model = LinearRegression()
-model.fit(X_train_normalized, y_train)
+if __name__ == '__main__':
+    files = []
+    file_names = ['data.csv', 'data2.csv', 'data3.csv', 'data4.csv', 'data5.csv']
+    for file_name in file_names:
+        files.append(pd.read_csv(file_name))
 
-mse = round(model.score(scaler.transform(X_test), y_test) * 100)
-print(f"Точность: {mse}%")
+    data = pd.concat(files, ignore_index=True).drop([
+        'Сумма по бил.',
+        'Сумма по плац.',
+        'Поезд/Нитка',
+        'Станция отп.',
+        'Станция назн.'
+    ], axis=1)
+    data = pd.get_dummies(data, columns=['Тип/Кл.обсл.'])
 
-new_data = pd.DataFrame({
-    'Дней до отправления': [5],
-    'Тип/Кл.обсл.': ['3Э'],
-    'Кол-во прод. мест': [1],
-    'Сумма серв. усл.': [400],
-    'Расстояние': [2500]
-})
+    X = data.drop('Итого сумма билета', axis=1)
+    y = data['Итого сумма билета']
 
-new_data_encoded = pd.get_dummies(new_data, columns=['Тип/Кл.обсл.'])
-new_data_encoded = new_data_encoded.reindex(columns=X.columns, fill_value=0)
-new_data_normalized = scaler.transform(new_data_encoded)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-predicted_price = round(model.predict(new_data_normalized)[0], 1)
-print(f"Предсказанная цена билета: {predicted_price}")
+    scaler = MinMaxScaler()
+    X_train_normalized = scaler.fit_transform(X_train)
+
+    model = LinearRegression()
+    model.fit(X_train_normalized, y_train)
+
+    mse = round(model.score(scaler.transform(X_test), y_test) * 100)
+    print(f"Точность: {mse}%")
+
+    # Save the trained model
+    with open('trained_model2.pkl', 'wb') as file:
+        pickle.dump((model, scaler, X.columns), file)
+
+    new_data = pd.DataFrame({
+        'Дней до отправления': [5],
+        'Тип/Кл.обсл.': ['3Э'],
+        'Кол-во прод. мест': [1],
+        'Сумма серв. усл.': [400],
+        'Расстояние': [2500]
+    })
+
+    predicted_price = predict_price(new_data)
+    print(f"Предсказанная цена билета: {predicted_price}")
